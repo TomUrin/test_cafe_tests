ad46f43bd94b5408f8535e87b52c67d3990834fe
TomasUri <urinovic.tomas@gmail.com>
2022-12-04 16:44:40 +0200

    Cart sum test

--- tests/cart_test.js
+++ tests/cart_test.js
@@ -1,15 +1,16 @@
 import { Selector, t } from "testcafe";
-import { ageVerification, cartCheck, firstItem, secondItem } from './functions';
+import { ageVerification, cartCheck, firstItem, secondItem, sumCheck } from './functions';
 
 fixture `Cart`
     .page `https://www.astuonkojis.lt`
     
-test("Errors", async t => {
+test("Cart", async t => {
 
     await ageVerification();
     await firstItem();
     await secondItem();
     await t
     .click(Selector('#cart-link'))
-    await cartCheck()
+    await cartCheck();
+    await sumCheck();
 });
--- tests/functions.js
+++ tests/functions.js
@@ -55,3 +55,27 @@ export async function cartCheck() {
     .expect(Selector('.item-title').nth(0).innerText).contains('Žemaičių blynai su mėsa, 1 kg')
     .expect(Selector('.item-title').nth(1).innerText).contains('Ekologiškas rožinis vynas be alkoholio UBY OSMOSE ROSE, 750 ml');
 };
+
+export async function sumCheck() {
+    // Items sum
+    const sum1String = await Selector('.price').nth(0).innerText;
+    const sum1 = parseFloat(sum1String) / 100;
+    const sum2String = await  Selector('.price').nth(1).innerText;
+    const sum2 = parseFloat(sum2String) / 100;
+    const itemsSum = Number(sum1.toFixed(2)) + Number(sum2.toFixed(2));
+    // Cart sum
+    const cartSumString = await Selector('.cart-summary-row-price').innerText;
+    const cartSum = parseFloat(cartSumString);
+    // Equal
+    await t
+    .expect(itemsSum).eql(Number(cartSum))
+    // Delete item
+    await t
+    .click(Selector('a').withText('Pašalinti'))
+    .click(Selector('.modal-confirm'))
+    // Item sum vs. cart sum check
+    const cartSum2 = await Selector('.cart-summary-row-price').innerText;
+    const finalCart2 = parseFloat(cartSum2);
+    await t
+    .expect(Number(cartSum)).notEql(Number(finalCart2))
+};
